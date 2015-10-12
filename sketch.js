b6bf408d444474fe91d4a03294f9150353d79788
Mike <michaelcmurphy12@gmail.com>
2015-10-12 01:54:45 -0400

    grid done, trouble with hover

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
 	for (var x = 0; x < 16; x++) {
 		for(var y = 0; y < 16; y++) {
-			var unit =$("<div class='unit'></div>");
+			var unit =$("<div class='startunit'></div>");
 			unit.appendTo('#container');
 			}
 		}
 
 	});
 
+//*function changeColor(){
+//	$(".startunit").addclass('.drawunit');
+//	}
+
+//$("<div class='startunit'>").mouseenter(
+//	changeColor();
+	
+//);
+//$("<div class='startunit'>").click(
+//	function changeColor(){
+//	$(".startunit").addclass('.clickedunit');
+//	}
+	
+//);
 
